73eaf3e541d1738ccb4638f992d4349b1feabd10
Ubuntu <ubuntu@ip-172-31-41-148.us-west-2.compute.internal>
2022-08-18 23:51:22 +0000

    MVP done

--- src/App.js
+++ src/App.js
@@ -7,26 +7,37 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Context
+import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart, item])
 	};
+
+	const removeItem = id => {
+		setCart(cart.filter(item => item.id !== id))
+	}
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{ products, addItem }} >
+				<CartContext.Provider value={{cart, removeItem}}>
+					<Navigation />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+					<Route exact path="/">
+						<Products />
+					</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
